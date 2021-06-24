39ea0ac07941a4764b10cb0601fae0955d29c711
박성재 <@210.178.237.23>
2021-06-24 22:50:08 +0900

    1111

---
+++ 20210624/src/메소드응용.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class 메소드응용 {
+
+	static int input(String s) {
+		Scanner scan=new Scanner(System.in);
+		System.out.printf("%s 입력:\n",s);
+		return scan.nextInt();
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		input("년");
+		input("월");
+	}
+
+}
